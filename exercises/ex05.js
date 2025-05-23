f06071c4a23c3df6313fa9d5fd69eb1468c77b8b
Bella9693 <realbella9693@gmail.com>
2025-05-23 15:05:34 -0700

    exercise 5 completed

--- exercises/ex05.js
+++ exercises/ex05.js
@@ -8,7 +8,9 @@ XL	    24"	    31"	    8.88"
 2XL	    26"	    33"	    9.63"
 3XL	    28"	    34"	    10.13"
 
-Use the sizing chart above, create a series of logical expressions that prints the size of a t-shirt based on the measurements of shirtWidth, shirtLength, and shirtSleeve. Valid sizes include S, M, L, XL, 2XL, and 3XL.
+Use the sizing chart above, create a series of logical expressions 
+that prints the size of a t-shirt based on the measurements of shirtWidth, shirtLength, and shirtSleeve. 
+Valid sizes include S, M, L, XL, 2XL, and 3XL.
 
 For example, if...
 
@@ -18,7 +20,9 @@ const shirtSleeve = 8.71; // size L (large)
 
 Then print L to the console. 
 
-If shirtWidth, shirtLength, and shirtSleeve don't fit within the range of acceptable values for a specific size, then print NA to the console. For example, if...
+If shirtWidth, shirtLength, and shirtSleeve don't fit 
+within the range of acceptable values for a specific size, then print NA to the console. 
+For example, if...
 
 var shirtWidth = 18; // size S (small)
 var shirtLength = 29; // size M (medium)
@@ -26,3 +30,67 @@ var shirtSleeve = 8.47; // size M (medium)
 Then print N/A to the console because the measurements don't all match up with one particular size.
 
 */
+// Size	   Width   Length  Sleeve
+// S	    18"	    28"	    8.13"
+// M	    20"	    29"	    8.38"
+// L	    22"	    30"	    8.63"
+// XL	    24"	    31"	    8.88"
+// 2XL	    26"	    33"	    9.63"
+// 3XL	    28"	    34"	    10.13"
+
+const shirtWidth = 22; // size L (large)
+const shirtLength = 30; // size L (large)
+const shirtSleeve = 8.62; // size L (large)
+
+let size = "N/A";
+
+if (shirtWidth <= 18 && shirtLength <= 28 && shirtSleeve <= 8.13) {
+  console.log("size S");
+} else if (
+  shirtWidth > 18 &&
+  shirtWidth <= 20 &&
+  shirtLength > 28 &&
+  shirtLength <= 29 &&
+  shirtSleeve > 8.13 &&
+  shirtSleeve <= 8.38
+) {
+  console.log("size M");
+} else if (
+  shirtWidth > 20 &&
+  shirtWidth <= 22 &&
+  shirtLength > 29 &&
+  shirtLength <= 30 &&
+  shirtSleeve > 8.38 &&
+  shirtSleeve <= 8.63
+) {
+  console.log("size L");
+} else if (
+  shirtWidth > 22 &&
+  shirtWidth <= 24 &&
+  shirtLength > 30 &&
+  shirtLength <= 31 &&
+  shirtSleeve > 8.63 &&
+  shirtSleeve <= 8.88
+) {
+  console.log("size XL");
+} else if (
+  shirtWidth > 24 &&
+  shirtWidth <= 26 &&
+  shirtLength > 31 &&
+  shirtLength <= 33 &&
+  shirtSleeve > 8.88 &&
+  shirtSleeve <= 9.63
+) {
+  console.log("size 2XL");
+} else if (
+  shirtWidth > 26 &&
+  shirtWidth <= 28 &&
+  shirtLength > 33 &&
+  shirtLength <= 34 &&
+  shirtSleeve > 9.63 &&
+  shirtSleeve <= 10.13
+) {
+  console.log("size 3XL");
+} else {
+  console.log(size);
+}
