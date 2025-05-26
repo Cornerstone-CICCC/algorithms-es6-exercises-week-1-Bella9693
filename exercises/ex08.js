b9846cdfdc1f06ff65bb099bf5ca9e9416855660
Bella9693 <realbella9693@gmail.com>
2025-05-26 14:57:32 -0700

    exercise 8 completed

--- exercises/ex08.js
+++ exercises/ex08.js
@@ -13,12 +13,12 @@ You can check if a number is divisible by another number by checking if x % y ==
 
 */
 for (let i = 1; i <= 100; i++) {
-  if (i % 3 === 0 && i % 5 === 0) {
-    console.log("FizzBuzz");
-  } else if (i % 3 === 0) {
+  if (i % 3 === 0) {
     console.log("Fizz");
   } else if (i % 5 === 0) {
     console.log("Buzz");
+  } else if (i % 3 === 0 && i % 5 === 0) {
+    console.log("FizzBuzz");
   } else {
     console.log(i);
   }
