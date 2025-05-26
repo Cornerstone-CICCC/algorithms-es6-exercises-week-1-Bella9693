2d55428acfbdeaf083887df002d849b2f41ed233
Bella9693 <realbella9693@gmail.com>
2025-05-25 19:42:55 -0700

    exercise 2 completed

--- exercises/ex02.js
+++ exercises/ex02.js
@@ -10,18 +10,18 @@ Write a series of conditional statements that:
 
 */
 
-let musicians = 7;
+let musicians = 5;
 
-if (musicians > 4) {
-  console.log("this is a large group");
-} else if ((musicians = 4)) {
-  console.log("quartet");
-} else if ((musicians = 3)) {
-  console.log("trio");
-} else if ((musicians = 2)) {
-  console.log("duet");
-} else if ((musicians = 1)) {
-  console.log("solo");
-} else {
+if (musicians <= 0) {
   console.log("not a group");
+} else if (musicians === 1) {
+  console.log("solo");
+} else if (musicians === 2) {
+  console.lo("duet");
+} else if (musicians === 3) {
+  console.log("trio");
+} else if (musicians === 4) {
+  console.log("quartet");
+} else {
+  console.log("this is a large group");
 }
