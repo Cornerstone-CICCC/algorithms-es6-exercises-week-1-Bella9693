4310c4d3966d32846861b9d5450204a1968d0688
Bella9693 <realbella9693@gmail.com>
2025-05-25 19:33:24 -0700

    exercise 1 completed

--- exercises/ex01.js
+++ exercises/ex01.js
@@ -8,7 +8,7 @@ Hint: Use the %(modulo) operator to determine is a number is even or odd.
 
 const number = 4; // Change your number to test your if statement
 
-if (number % 2 == 0) {
+if (number % 2 === 0) {
   console.log("even");
 } else {
   console.log("odd");
